6bc2bfdbf4aa3da58c36982ff380b67b85c55431
ViniciusSito <144069714+ViniciusSito@users.noreply.github.com>
2023-11-24 17:39:00 -0300

    Update Cartao.cs

--- Models/Cartao.cs
+++ Models/Cartao.cs
@@ -1,4 +1,4 @@
-// Juan 
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -7,13 +7,13 @@ namespace BancoAPI.Models;
     public class CartaoCredito
     {   
         [Key]
-        public int Id {get; set;}
+        public int? Id {get; set;}
 
-        public string NumeroCartao {get; set;}
-        public DateTime DataValidade {get; set;}
-        public string CodigoSeguranca {get; set;}
-        public decimal Limite {get; set;}
-        public bool Bloqueado {get; set;}
+        public string? NumeroCartao {get; set;}
+        public DateTime? DataValidade {get; set;}
+        public string? CodigoSeguranca {get; set;}
+        public decimal? Limite {get; set;}
+        public bool? Bloqueado {get; set;}
 
         public int ContaID {get; set;}
         
